42e661e89adc9be0d5fd0bd5492354d8f8f4998a
joaovito9 <joaovm.2000@hotmail.com>
2024-11-13 23:36:11 -0300

    properties ajustado, Game entity criado, ORM

---
+++ src/main/java/com/joaovitor/listagames/entities/Game.java
@@ -0,0 +1,138 @@
+package com.joaovitor.listagames.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Integer ano;
+
+    private String genero;
+
+    private String plataforma;
+
+    private Double score;
+
+    private String imgUrl;
+
+    private String descrisaoCurta;
+
+    private String descrisaoLonga;
+
+//    Tudo feito com Lombok, comentario só para demonstrar.
+//    Construtor sem args.
+//    public Game() {
+//    }
+//
+//    Construtor com args.
+//    public Game(Long id, String nome, Integer ano, String genero, String plataforma, Double score, String imgUrl, String descrisaoCurta, String descrisaoLonga) {
+//        this.id = id;
+//        this.nome = nome;
+//        this.ano = ano;
+//        this.genero = genero;
+//        this.plataforma = plataforma;
+//        this.score = score;
+//        this.imgUrl = imgUrl;
+//        this.descrisaoCurta = descrisaoCurta;
+//        this.descrisaoLonga = descrisaoLonga;
+//    }
+//
+//    Getters e Setters
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public Integer getAno() {
+//        return ano;
+//    }
+//
+//    public void setAno(Integer ano) {
+//        this.ano = ano;
+//    }
+//
+//    public String getGenero() {
+//        return genero;
+//    }
+//
+//    public void setGenero(String genero) {
+//        this.genero = genero;
+//    }
+//
+//    public String getPlataforma() {
+//        return plataforma;
+//    }
+//
+//    public void setPlataforma(String plataforma) {
+//        this.plataforma = plataforma;
+//    }
+//
+//    public Double getScore() {
+//        return score;
+//    }
+//
+//    public void setScore(Double score) {
+//        this.score = score;
+//    }
+//
+//    public String getImgUrl() {
+//        return imgUrl;
+//    }
+//
+//    public void setImgUrl(String imgUrl) {
+//        this.imgUrl = imgUrl;
+//    }
+//
+//    public String getDescrisaoCurta() {
+//        return descrisaoCurta;
+//    }
+//
+//    public void setDescrisaoCurta(String descrisaoCurta) {
+//        this.descrisaoCurta = descrisaoCurta;
+//    }
+//
+//    public String getDescrisaoLonga() {
+//        return descrisaoLonga;
+//    }
+//
+//    public void setDescrisaoLonga(String descrisaoLonga) {
+//        this.descrisaoLonga = descrisaoLonga;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(id);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Game game = (Game) o;
+//        return Objects.equals(id, game.id);
+//    }
+}
