cc38606357100f83a3fd76955162dea724f824cf
joaovito9 <joaovm.2000@hotmail.com>
2024-11-13 21:38:52 -0300

    Projeto criado.

---
+++ src/main/java/com/joaovitor/listagames/ListagamesApplication.java
@@ -0,0 +1,13 @@
+package com.joaovitor.listagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListagamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListagamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovitor/listagames/ListagamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovitor.listagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListagamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
