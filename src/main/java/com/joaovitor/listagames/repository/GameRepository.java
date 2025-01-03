bc45aa41083d23e4d911568e3e09d94922038015
joaov <joaovm.2000@hotmail.com>
2025-01-02 23:36:39 -0300

    Deploy pronto.

--- src/main/java/com/joaovitor/listagames/repository/GameRepository.java
+++ src/main/java/com/joaovitor/listagames/repository/GameRepository.java
@@ -10,10 +10,16 @@ import java.util.List;
 public interface GameRepository extends JpaRepository<Game, Long> {
 
     @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.titulo, tb_game.ano, tb_game.img_url AS imgUrl,
-		tb_game.descrisao_curta, tb_pertence.posicao
+		SELECT tb_game.id,
+		 	   tb_game.titulo,
+		 	   tb_game.ano,
+		 	   tb_game.img_url AS imgUrl,
+			   tb_game.descrisao_curta, 
+			   tb_pertence.posicao
 		FROM tb_game
-		INNER JOIN tb_pertence ON tb_game.id = tb_pertence.game_id
+		INNER JOIN tb_pertence 
+			ON 
+		tb_game.id = tb_pertence.game_id
 		WHERE tb_pertence.lista_id = :listId
 		ORDER BY tb_pertence.posicao
 			""")
